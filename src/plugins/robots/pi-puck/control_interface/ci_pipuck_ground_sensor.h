7d723417b8744a61edf20373bb2924eb723e528b
Charlotte Wringe <cw1647@york.ac.uk>
2020-07-09 13:32:54 +0100

    changed the cpp controller to make it fit better

--- src/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_sensor.h
+++ src/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_sensor.h
@@ -39,83 +39,6 @@ namespace argos {
          Real Reading;
       };
 
-      struct SReadings {
-         /**
-          * Left reagind
-          */
-         Real Left;
-
-         /**
-          * Center reading
-          */
-         Real Center;
-
-         /**
-          * Right reading
-          */
-         Real Right;
-
-         /**
-          * Empty constructor
-          */
-         SReadings() :
-            Left(0.0f),
-            Center(0.0f),
-            Right(0.0f) {}
-
-         /**
-          * Reset the reading (ie. all 0)
-          */
-         void Reset() {
-            Left = 0.0f;
-            Center = 0.0f;
-            Right = 0.0f;
-         }
-         
-                  /**
-          * Define a new operator [] to allow accessing to readings like an array
-          *
-          * @param un_idx id of the field to acces. 0 -> Left, 1 -> Center and 2 -> Right
-          */
-         Real& operator[] (size_t un_idx) {
-            if(un_idx == 0) return Left;
-            else if(un_idx == 1) return Center;
-            else if(un_idx == 2) return Right;
-            else THROW_ARGOSEXCEPTION("Index of out bound accessing the ground sensor reading. "
-                                      << "Expected [0-2], got: " << un_idx);
-         }
-
-        Real const& operator[] (size_t un_idx) const {
-            if(un_idx == 0) return Left;
-            else if(un_idx == 1) return Center;
-            else if(un_idx == 2) return Right;
-            else THROW_ARGOSEXCEPTION("Index of out bound accessing the ground sensor reading. "
-                                      << "Expected [0-2], got: " << un_idx);
-         }
-      };
-
-      /**
-       * Returns the sensor data as a SReadings.
-       * @return the sensor data as a SReadings
-       */
-      inline const SReadings& GetReadings() const {
-         return m_sReadings;
-      }
-
-      /**
-       * Returns the sensor data as an array of 3 elements.
-       * @return the sensor data as an array of 3 elements.
-       */
-      inline void GetReadings(Real* pf_buffer) const {
-         pf_buffer[0] = m_sReadings.Left;
-         pf_buffer[1] = m_sReadings.Center;
-         pf_buffer[2] = m_sReadings.Right;
-      }
-
-      inline Real GetReading(size_t un_idx) {
-         return m_sReadings[un_idx];
-      }
-
       virtual ~CCI_PiPuckGroundSensor() {}
 
       virtual void ForEachInterface(std::function<void(const SInterface&)>) = 0;
@@ -130,7 +53,6 @@ namespace argos {
 
       static const std::map<std::string, TConfiguration> m_mapSensorConfig;
 
-      SReadings m_sReadings;
 
    }; 
 
--- src/testing/pi-puck/simulator/controllers/test_pipuck_cpp.cpp
+++ src/testing/pi-puck/simulator/controllers/test_pipuck_cpp.cpp
@@ -2,7 +2,9 @@
 #include <argos3/core/utility/configuration/argos_configuration.h>
 
 CTestPipuckCpp::CTestPipuckCpp() :
-   m_pcLEDs(NULL)
+   m_pcLEDs(NULL),
+   m_pcDrive(NULL),
+   m_pcGround(NULL)
    {}
 
 void CTestPipuckCpp::Init(TConfigurationNode& t_node) {
@@ -21,8 +23,52 @@ void CTestPipuckCpp::Reset() {
 }
 
 void CTestPipuckCpp::ControlStep() {
-   Real breadings[3];
-   m_pcGround->GetReadings(breadings);
+   std::vector<Real> vecReadings;
+   Real Readings[3];
+   size_t i = 0;
+   m_pcGround->ForEachInterface([&vecReadings, &Readings, &i] (const CCI_PiPuckGroundSensor::SInterface& s_interface)
+   {
+      vecReadings.emplace_back(s_interface.Reading);
+      std::cout << "Label: " << s_interface.Label << std::endl;
+      std::cout << "Reading: " << s_interface.Reading << std::endl;
+      Readings[i%3] = s_interface.Reading;
+      i ++;
+
+      const CCI_PiPuckGroundSensor::TConfiguration configuration = s_interface.Configuration;
+      std::cout << "Name: " << std::get<0>(configuration) << std::endl;
+      std::cout << "Position: " << std::get<1>(configuration) << std::endl;
+      std::cout << "Orientation: " << std::get<2>(configuration) << std::endl;
+      std::cout << "Range: " << std::get<3>(configuration) << std::endl;
+
+      std::cout << std::endl;
+
+   }
+   );
+   if(Readings[2] < 0.6) {
+      if(Readings[0] < 0.6) {
+         m_pcDrive->SetTargetVelocityLeft(0.01);
+         m_pcDrive->SetTargetVelocityRight(-0.00); 
+         std::cout << "right"  << std::endl;
+      } else {
+         m_pcDrive->SetTargetVelocityLeft(0.01);
+         m_pcDrive->SetTargetVelocityRight(0.01); 
+         std::cout << "hard right" << std::endl;
+      }
+   } else if(Readings[1] < 0.6) {
+      if(Readings[0] < 0.6) {
+         m_pcDrive->SetTargetVelocityLeft(0.00);
+         m_pcDrive->SetTargetVelocityRight(-0.01);  
+         std::cout << "left" << std::endl; 
+      } else {
+         m_pcDrive->SetTargetVelocityLeft(-0.01);
+         m_pcDrive->SetTargetVelocityRight(-0.01);  
+         std::cout << "hard left" << std::endl; 
+      }
+   } else {
+      m_pcDrive->SetTargetVelocityLeft(0.005);
+      m_pcDrive->SetTargetVelocityRight(-0.005);
+      std::cout << "straight ahead" <<std::endl;
+   }
 
    if(count < 10) {
       count ++;
@@ -31,24 +77,6 @@ void CTestPipuckCpp::ControlStep() {
       count = 1;
       m_pcLEDs->SetAllColors(CColor::BLACK);
    }
-   if(breadings[2] < 0.6) {
-      if(breadings[1] < 0.6) {
-         m_pcDrive->SetTargetVelocityLeft(0.01);
-         m_pcDrive->SetTargetVelocityRight(0.00);   
-      } else {
-         m_pcDrive->SetTargetVelocityLeft(0.01);
-         m_pcDrive->SetTargetVelocityRight(-0.01); 
-      }
-   } else if(breadings[0] < 0.6) {
-      if(breadings[1] < 0.6) {
-         m_pcDrive->SetTargetVelocityLeft(0.00);
-         m_pcDrive->SetTargetVelocityRight(0.01);   
-      } else {
-         m_pcDrive->SetTargetVelocityLeft(-0.01);
-         m_pcDrive->SetTargetVelocityRight(0.01);   
-      }
-   }
-   
 
 
 }
