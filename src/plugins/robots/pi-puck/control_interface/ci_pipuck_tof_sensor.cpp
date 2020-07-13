0ae572972b49861340ed93107dac9a96beb019b0
Charlotte Wringe <cw1647@york.ac.uk>
2020-07-13 10:13:18 +0100

    added a time-of-flight sensor

---
+++ src/plugins/robots/pi-puck/control_interface/ci_pipuck_tof_sensor.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_tof_sensor.cpp>
+ *
+ * @author C Wringe <uint8.lotte@gmail.com>
+ */
+
+#include "ci_pipuck_tof_sensor.h"
+
+#ifdef ARGOS_WITH_LUA
+#include <argos3/core/wrappers/lua/lua_utility.h>
+#endif
+
+namespace argos {
+
+   /****************************************/
+   /****************************************/
+
+#ifdef ARGOS_WITH_LUA
+   void CCI_PiPuckTofSensor::CreateLuaState(lua_State* pt_lua_state) {
+      CLuaUtility::StartTable(pt_lua_state, "tof");
+      ForEachInterface([pt_lua_state] (const SInterface& s_interface) {
+         CLuaUtility::StartTable(pt_lua_state, s_interface.Label);
+         CLuaUtility::AddToTable(pt_lua_state, "reading", s_interface.Reading);
+         CLuaUtility::StartTable(pt_lua_state, "transform");
+         CLuaUtility::AddToTable(pt_lua_state, "position", std::get<CVector3>(s_interface.Configuration));
+         CLuaUtility::AddToTable(pt_lua_state, "orientation", std::get<CQuaternion>(s_interface.Configuration));
+         CLuaUtility::AddToTable(pt_lua_state, "anchor", std::get<const char*>(s_interface.Configuration));
+         CLuaUtility::EndTable(pt_lua_state);
+         CLuaUtility::EndTable(pt_lua_state);
+      });
+      CLuaUtility::EndTable(pt_lua_state);
+   }
+#endif
+
+   /****************************************/
+   /****************************************/
+
+#ifdef ARGOS_WITH_LUA
+   void CCI_PiPuckTofSensor::ReadingsToLuaState(lua_State* pt_lua_state) {
+      lua_getfield(pt_lua_state, -1, "tof");
+      ForEachInterface([pt_lua_state] (const SInterface& s_interface) {
+         lua_pushnumber(pt_lua_state, s_interface.Label);
+         lua_gettable(pt_lua_state, -2);
+         lua_pushstring(pt_lua_state, "reading");
+         lua_pushnumber(pt_lua_state, s_interface.Reading);
+         lua_settable(pt_lua_state, -3);
+         lua_pop(pt_lua_state, 1);
+      });
+      lua_pop(pt_lua_state, 1);
+   }
+#endif
+
+   /****************************************/
+   /****************************************/
+
+   /* taken from JA's specification */
+   /* height is a complete and utter guess, and mostly just there to make sure I have _something_. TODO: check & fix this*/
+
+   const std::map<UInt8, CCI_PiPuckTofSensor::TConfiguration > CCI_PiPuckTofSensor::m_mapSensorConfig = {
+      std::make_pair(1,  std::make_tuple("origin", CVector3( 0.000, 0.030, 0.05), CQuaternion( -0.5 * CRadians::PI, CVector3(1.0, 00, 0)))), 
+      std::make_pair(2,  std::make_tuple("origin", CVector3( 0.023, 0.023, 0.05), CQuaternion( -0.5 * CRadians::PI, CVector3(0.730, -0.683,0)))),
+      std::make_pair(3,  std::make_tuple("origin", CVector3( 0.023, -0.023, 0.05), CQuaternion( 0.5 * CRadians::PI, CVector3(0.73, 0.683,0)))),
+      std::make_pair(4,  std::make_tuple("origin", CVector3( 0.000, -.030, 0.05), CQuaternion( 0.5 * CRadians::PI, CVector3(1.0, 0, 0)))), 
+      std::make_pair(5,  std::make_tuple("origin", CVector3(-0.023, -0.023, 0.05), CQuaternion( 0.5 * CRadians::PI, CVector3(0.73, -0.683,0)))),
+      std::make_pair(6,  std::make_tuple("origin", CVector3( -0.023, 0.023, 0.05), CQuaternion( -0.5 * CRadians::PI, CVector3(0.73,0.683,0)))),
+   };
+
+   /****************************************/
+   /****************************************/
+}
---
+++ src/plugins/robots/pi-puck/control_interface/ci_pipuck_tof_sensor.h
@@ -0,0 +1,62 @@
+/**
+ * @file <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_tof_sensor.h>
+ *
+ * @author C Wringe <uint8.lotte@gmail.com>
+ */
+
+#ifndef CCI_PIPUCK_TOF_SENSOR_H
+#define CCI_PIPUCK_TOF_SENSOR_H
+
+namespace argos {
+    class CCI_PiPuckTofSensor;
+}
+
+#include <argos3/core/control_interface/ci_sensor.h>
+#include <argos3/core/utility/math/vector3.h>
+#include <argos3/core/utility/math/quaternion.h>
+
+#include <map>
+#include <tuple>
+#include <functional>
+
+namespace argos {
+   
+   class CCI_PiPuckTofSensor : public CCI_Sensor {
+   
+
+   public:
+      
+
+      using TConfiguration = std::tuple<const char*, CVector3, CQuaternion>;
+
+      struct SInterface {
+          SInterface(const UInt8& un_label) :
+             Label(un_label),
+             Configuration(m_mapSensorConfig.at(un_label)),
+             Reading(0.0f) {}
+         const UInt8& Label;
+         const TConfiguration& Configuration;
+         Real Range;
+         Real Reading;
+      };
+
+      virtual ~CCI_PiPuckTofSensor() {}
+
+      virtual void Init(TConfigurationNode& t_tree);
+
+      virtual void ForEachInterface(std::function<void(const SInterface&)>) = 0;
+
+#ifdef ARGOS_WITH_LUA
+      virtual void CreateLuaState(lua_State* pt_lua_state);
+
+      virtual void ReadingsToLuaState(lua_State* pt_lua_state);
+
+#endif
+   protected:
+
+      static const std::map<UInt8, TConfiguration> m_mapSensorConfig;
+
+   };
+}
+
+#endif
